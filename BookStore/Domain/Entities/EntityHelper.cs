d89c58c141d98d19783ebaa175880fa749623e06
nhatran03 <tranthanhnha84@gmail.com>
2019-03-07 22:18:41 -0600

    implement Reflection

---
+++ BookStore/Domain/Entities/EntityHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BookStore.Domain.Entities
+{
+	/// <summary>
+	/// Some helper methods for entities.
+	/// </summary>
+	public static class EntityHelper
+	{
+		public static bool IsEntity(Type type)
+		{
+			return true;
+		}
+	}
+}
---
+++ BookStore/Reflection/AssemblyHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace BookStore.Reflection.Extensions
+{
+	internal static class AssemblyHelper
+	{
+		public static List<Assembly> GetAllAssembliesInFolder(string folderPath, SearchOption searchOption)
+		{
+			var essemblyFiles = Directory
+				.EnumerateFiles(folderPath, "*.*", searchOption)
+				.Where(s => s.EndsWith(".dll") || s.EndsWith(".exe"));
+
+			return essemblyFiles.Select(
+				Assembly.LoadFile
+				).ToList();
+		}
+	}
+}
