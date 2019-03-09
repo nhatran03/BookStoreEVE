299149688ff6072876696a4f2e031a8873f032ef
nhatran03 <tranthanhnha84@gmail.com>
2019-03-09 17:35:05 -0600

    continue implemented Reflection

---
+++ BookStore/Reflection/TypeFinder.cs
@@ -0,0 +1,86 @@
+﻿using Castle.Core.Internal;
+using Castle.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BookStore.Reflection
+{
+	public class TypeFinder : ITypeFinder
+	{
+
+		public ILogger Logger { get; set; }
+
+		private readonly IAssemblyFinder assemblyFinder;
+		private readonly object syncObj = new object();
+		private Type[] types;
+
+		public TypeFinder(IAssemblyFinder assemblyFinder)
+		{
+			this.assemblyFinder = assemblyFinder;
+			Logger = NullLogger.Instance;
+		}
+		public Type[] Find(Func<Type, bool> predicate)
+		{
+
+			throw new NotImplementedException();
+		}
+
+		public Type[] FindAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		private Type[] GetAllTypes()
+		{
+			if(types == null)
+			{
+				lock (syncObj)
+				{
+					if(types == null)
+					{
+						types = CreateTypeList().ToArray();
+					}
+				}
+			}
+
+			return types;
+		}
+		private List<Type> CreateTypeList()
+		{
+			var allTypes = new List<Type>();
+			var assemblies = assemblyFinder.GetAllAssemblies().Distinct();
+
+			foreach(var assembly in assemblies)
+			{
+				try
+				{
+
+					Type[] typesInThisAssembly;
+					try
+					{
+						typesInThisAssembly = assembly.GetTypes();
+					}
+					catch (ReflectionTypeLoadException ex)
+					{
+						typesInThisAssembly = ex.Types;
+					}
+
+					if (typesInThisAssembly.IsNullOrEmpty())
+					{
+						continue;
+					}
+
+					allTypes.AddRange(typesInThisAssembly.Where(type => type != null));
+				}
+				catch (Exception ex)
+				{
+					Logger.Warn(ex.ToString(), ex);
+				}
+			}
+
+			return allTypes;
+		}
+	}
+}
---
+++ BookStore/Reflection/TypeHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection;
+
+namespace BookStore.Reflection
+{
+	/// <summary>
+	/// Some simple type-checking methods used internally.
+	/// </summary>
+	internal static class TypeHelper
+	{
+		/// <summary>
+		/// Some simple type-checking methods used internally.
+		/// </summary>
+		public static bool IsFunc(object obj)
+		{
+			if(obj == null)
+			{
+				return false;
+			}
+
+			var type = obj.GetType();
+			if (!type.GetTypeInfo().IsGenericType)
+			{
+				return false;
+			}
+
+			return type.GetGenericTypeDefinition() == typeof(Func<>);
+		}
+
+		public static bool IsFunc<TReturn>(object obj)
+		{
+			return obj != null && obj.GetType() == typeof(Func<TReturn>);
+		}
+
+		public static bool IsPrimitiveExtendedIncludingNullable(Type type)
+		{
+			if (IsPrimitiveExtended(type))
+			{
+				return true;
+			}
+
+			if(type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+			{
+				return IsPrimitiveExtended(type.GenericTypeArguments[0]);
+			}
+
+			return false;
+		}
+
+		public static bool IsPrimitiveExtended(Type type)
+		{
+			if (type.GetTypeInfo().IsPrimitive)
+			{
+				return true;
+			}
+
+			return	type == typeof(string) ||
+					type == typeof(Decimal) ||
+					type == typeof(DateTime) ||
+					type == typeof(DateTimeOffset) ||
+					type == typeof(TimeSpan) ||
+					type == typeof(Guid);
+		}
+	}
+}
